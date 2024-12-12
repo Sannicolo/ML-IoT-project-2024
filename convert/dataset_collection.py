2212300a067f2c214bd894904a80256fb619baad
Victor Sannicolo <victor.sannicolo.5554@student.lu.se>
2024-12-12 16:08:56 +0100

    Co-authored-by: emelietingberg <emelietingberg@users.noreply.github.com>

--- convert/dataset_collection.py
+++ convert/dataset_collection.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import serial
@@ -19,6 +20,7 @@ def read_serial_data():
             print("second")
             data = list(map(float, line.split('[')[1].split(']')[0].split(',')))
             print(data)
+            break
         elif line.startswith("Classified class"):
             classified_class = int(line.split()[-1])
             break
@@ -26,6 +28,9 @@ def read_serial_data():
 
 plt.ion()  # Turn on interactive mode
 fig, ax = plt.subplots()
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 while True:
     # Read data from the serial port
@@ -43,3 +48,18 @@ while True:
 
     plt.draw()
     plt.pause(0.1)  # Pause to allow the plot to update
+
+    # Ask for user input to accept or reject the data
+    user_input = input("Enter 1 to accept or 2 to reject: ")
+    if user_input == '1':
+        # Save the data to a text file
+        with open(os.path.join(script_dir, 'dataBanana.txt'), 'a') as f:
+            f.write(','.join(map(str, data)) + '\n')
+
+        # Save the float arrays to a separate text file in the desired format
+        with open(os.path.join(script_dir, 'float_arraysBanana.txt'), 'a') as f:
+            f.write('{' + ','.join(map(str, data)) + '},\n')
+    elif user_input == '2':
+        print("Data rejected.")
+    else:
+        print("Invalid input. Data rejected.")
