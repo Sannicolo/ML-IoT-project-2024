fca8ccab5236a2eab413554d8ac3f6e6ec394020
Victor Sannicolo <victor.sannicolo.5554@student.lu.se>
2024-12-18 08:37:47 +0100

    Two way federated learning

--- src/BLE_Communication.h
+++ src/BLE_Communication.h
@@ -6,35 +6,42 @@ BLECharacteristic PeripheralCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214
 void sendData(BLEDevice peripheral, float *WeightBiasPtr);
 void BLECentralSetup(float *WeightBiasPtr);
 void CentralSearch(float *WeightBiasPtr);
-void BLEPeripheralSetup( float *WeightBiasPtr);
+void BLEPeripheralSetup(float *WeightBiasPtr);
 void PeripheralLoop(float *WeightBiasPtr);
 
-void sendData(BLEDevice peripheral, float *WeightBiasPtr) {
+void sendData(BLEDevice peripheral, float *WeightBiasPtr)
+{
   // connect to the peripheral
   Serial.println("Connecting ...");
 
-  if (peripheral.connect()) {
+  if (peripheral.connect())
+  {
 
-    if (peripheral.discoverAttributes()) {
+    if (peripheral.discoverAttributes())
+    {
       Serial.println("Attribute discovery succeded!");
     }
-    else {
+    else
+    {
       Serial.println("Attribute discovery failed!");
       peripheral.disconnect();
       return;
     }
 
-    if (peripheral.localName() == "Peripheral") {
+    if (peripheral.localName() == "Peripheral")
+    {
       BLEService peripheralService = peripheral.service("19B10000-E8F2-537E-4F6C-D104768A1214");
 
-      if (peripheralService) {
+      if (peripheralService)
+      {
         Serial.println("Found peripheral service");
         BLECharacteristic peripheralCharacteristic = peripheralService.characteristic("19B10001-E8F2-537E-4F6C-D104768A1214");
 
-        if (peripheralCharacteristic) {
+        if (peripheralCharacteristic)
+        {
           Serial.println("Found peripheral switch characteristic");
-          for (int i = 0; i < 5972; i++) {
-            Serial.println(WeightBiasPtr[i]);
+          for (int i = 1; i <= 5972; i++)
+          {
             uint8_t byteArray[sizeof(float)];
             memcpy(byteArray, &WeightBiasPtr[i], sizeof(float));
             delay(10);
@@ -51,13 +58,19 @@ void sendData(BLEDevice peripheral, float *WeightBiasPtr) {
             }
             Serial.println(); */
 
+            Serial.print("Sending value: ");
+            Serial.print(i);
+            Serial.print(" ");
+            Serial.println(WeightBiasPtr[i]);
+
             peripheralCharacteristic.writeValue(byteArray, sizeof(float));
           }
         }
       }
     }
   }
-  else {
+  else
+  {
     Serial.println("Failed to connect!");
     return;
   }
@@ -67,30 +80,38 @@ void sendData(BLEDevice peripheral, float *WeightBiasPtr) {
   Serial.println("Disconnected");
 }
 
-void BLECentralSetup(float *WeightBiasPtr) {
+void BLECentralSetup(float *WeightBiasPtr)
+{
 
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial)
+    ;
 
-  if (!BLE.begin()) {
+  if (!BLE.begin())
+  {
     Serial.println("Starting Bluetooth Low Energy failed!");
 
-    while (1);
+    while (1)
+      ;
   }
 
   BLE.scan();
   CentralSearch(WeightBiasPtr);
 }
 
-void BLEPeripheralSetup(float *WeightBiasPtr) {
+void BLEPeripheralSetup(float *WeightBiasPtr)
+{
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial)
+    ;
 
   // begin initialization
-  if (!BLE.begin()){
+  if (!BLE.begin())
+  {
     Serial.println("starting Bluetooth Low Energy failed!");
 
-    while (1);
+    while (1)
+      ;
   }
 
   BLE.setLocalName("Peripheral");
@@ -98,7 +119,7 @@ void BLEPeripheralSetup(float *WeightBiasPtr) {
 
   PeripheralService.addCharacteristic(PeripheralCharacteristic);
   BLE.addService(PeripheralService);
-  //PeripheralCharacteristic.writeValue(0);
+  // PeripheralCharacteristic.writeValue(0);
   BLE.advertise();
 
   Serial.println("BLE Peripheral");
@@ -106,24 +127,29 @@ void BLEPeripheralSetup(float *WeightBiasPtr) {
   PeripheralLoop(WeightBiasPtr);
 }
 
-void PeripheralLoop(float *WeightBiasPtr) {
+void PeripheralLoop(float *WeightBiasPtr)
+{
   Serial.println("Peripheral Loop");
   bool transferComplete = false;
 
-  while (!transferComplete) {
+  while (!transferComplete)
+  {
     // listen for Bluetooth Low Energy peripherals to connect:
     BLEDevice central = BLE.central();
 
     // if a central is connected to peripheral:
-    if (central) {
+    if (central)
+    {
       Serial.print("Connected to central: ");
       Serial.println(central.address());
 
       // while the central is still connected to peripheral:
-      int count = 0; 
-      while (central.connected()) {
-      
-        if (PeripheralCharacteristic.written()) {
+      int count = 1;
+      while (central.connected())
+      {
+
+        if (PeripheralCharacteristic.written())
+        {
           // Serial.println(PeripheralCharacteristic.value());
           //  Assuming you have the byte array received from the peripheralCharacteristic
           //  Assuming you have a method to get the value as a byte array
@@ -132,7 +158,7 @@ void PeripheralLoop(float *WeightBiasPtr) {
           PeripheralCharacteristic.readValue(byteArray, sizeof(float));
 
           // Debug: Print byte array content after reception
-          //Serial.print("Received byte array: ");
+          // Serial.print("Received byte array: ");
           /* for (int i = 0; i < sizeof(float); i++) {
             uint8_t byteValue = byteArray[i];
             if (byteValue < 0x10) {
@@ -148,7 +174,8 @@ void PeripheralLoop(float *WeightBiasPtr) {
           memcpy(&receivedFloat, byteArray, sizeof(float));
 
           // Print the received float value
-
+          
+          Serial.print("Receiving value: ");
           Serial.print(count);
           Serial.print(" ");
           Serial.println(receivedFloat);
@@ -164,24 +191,29 @@ void PeripheralLoop(float *WeightBiasPtr) {
   }
 }
 
-void CentralSearch(float *WeightBiasPtr) {
+void CentralSearch(float *WeightBiasPtr)
+{
   bool foundPeripheral = false;
-  while (!foundPeripheral) {
-    Serial.println("Searching...");
+  Serial.println("Searching...");
+  while (!foundPeripheral)
+  {
 
     // check if a peripheral has been discovered
     BLEDevice peripheral = BLE.available();
 
-    if (peripheral) {
+    if (peripheral)
+    {
       Serial.print(peripheral.localName());
-      if (peripheral.localName() == "Peripheral") {
+      if (peripheral.localName() == "Peripheral")
+      {
         BLE.stopScan();
         Serial.println("Peripheral found. Connecting ...");
 
         sendData(peripheral, WeightBiasPtr);
         // foundPeripheral = true;
         //  peripheral disconnected
-        while (1);
+        while (1)
+          ;
       }
     }
   }
--- src/project.cpp
+++ src/project.cpp
@@ -8,15 +8,16 @@
 #define LEARNING_RATE 0.01 // The learning rate used to train your network
 #define EPOCH 50           // The maximum number of epochs
 #define DATA_TYPE_FLOAT    // The data type used: Set this to DATA_TYPE_DOUBLE for higher precision. However, it is better to keep this Float if you want to submit the result via BT
-#define EPOCH_RUN 1       // epochs to run
+#define EPOCH_RUN 9        // epochs to run during local training
 extern const int first_layer_input_cnt;
 extern const int classes_cnt;
 
+// Camera parameters and image processing
 byte image[160 * 120 * 2]; // QCIF: 176x144 x 2 bytes per pixel (RGB565)
 float resizedImage[24 * 24];
 int bytesPerFrame;
 
-int testSend = 8; 
+#define CENTRAL_OR_PERIPHERAL 0 // 0 for central, 1 for peripheral
 
 // You define your network in NN_def
 // Right now, the network consists of three layers:
@@ -24,15 +25,17 @@ int testSend = 8;
 // 2. A hidden layer with 20 nodes
 // 3. An output layer with as many classes as you defined in the variable classes_cnt in cnn_data.h
 static const unsigned int NN_def[] = {first_layer_input_cnt, 10, classes_cnt};
-
 #include "data-more-test.h" // The data, labels and the sizes of all objects are stored here
 #include "NN_functions.h"   // All NN functions are stored here
 
 int iter_cnt = 0; // This keeps track of the number of epochs you've trained on the Arduino
 #define DEBUG 0   // This prints the weights of your network in case you want to do debugging (set to 1 if you want to see that)
 
-void convertToGrayscale(byte *image) {
-  for (int i = 0; i < bytesPerFrame; i += 2) {
+// Converts the RGB565 image to grayscale
+void convertToGrayscale(byte *image)
+{
+  for (int i = 0; i < bytesPerFrame; i += 2)
+  {
     uint16_t pixel = (image[i] << 8) | image[i + 1];
     uint8_t r = (pixel >> 11) & 0x1F;
     uint8_t g = (pixel >> 5) & 0x3F;
@@ -42,15 +45,19 @@ void convertToGrayscale(byte *image) {
   }
 }
 
-void resizeImage() {
+// Resizes the image to desired size (24x24)
+void resizeImage()
+{
   int srcWidth = 160;
   int srcHeight = 120;
   int destWidth = 24;
   int destHeight = 24;
   float xRatio = srcWidth / (float)destWidth;
   float yRatio = srcHeight / (float)destHeight;
-  for (int y = 0; y < destHeight; y++) {
-    for (int x = 0; x < destWidth; x++) {
+  for (int y = 0; y < destHeight; y++)
+  {
+    for (int x = 0; x < destWidth; x++)
+    {
       int srcX = (int)(x * xRatio);
       int srcY = (int)(y * yRatio);
       resizedImage[y * destWidth + x] = image[srcY * srcWidth + srcX] / 255.0;
@@ -58,11 +65,15 @@ void resizeImage() {
   }
 }
 
-void printFloatArray() {
+// Prints the image array
+void printFloatArray()
+{
   Serial.print("[");
-  for (int i = 0; i < 24 * 24; i++) {
+  for (int i = 0; i < 24 * 24; i++)
+  {
     Serial.print(resizedImage[i], 3);
-    if (i != 24 * 24 - 1) {
+    if (i != 24 * 24 - 1)
+    {
       Serial.print(", ");
     }
   }
@@ -70,7 +81,8 @@ void printFloatArray() {
 }
 
 // This function contains your training loop
-void do_training() {
+void do_training()
+{
 
   // Print the epoch number
   Serial.print("Epoch count (training count): ");
@@ -81,7 +93,8 @@ void do_training() {
   shuffleIndx();
 
   // starting forward + Backward propagation
-  for (int j = 0; j < numTrainData; j++) {
+  for (int j = 0; j < numTrainData; j++)
+  {
     generateTrainVectors(j);
     forwardProp();
     backwardProp();
@@ -91,18 +104,22 @@ void do_training() {
   printAccuracy();
 }
 
-void classifyImage() {
-  for (int i = 0; i < IN_VEC_SIZE; i++) {
+void classifyImage()
+{
+  for (int i = 0; i < IN_VEC_SIZE; i++)
+  {
     input[i] = resizedImage[i];
   }
 
   forwardProp();
   int maxIndx = 0;
 
-  if (y[0] > y[1]) {
+  if (y[0] > y[1])
+  {
     maxIndx = 0;
   }
-  else {
+  else
+  {
     maxIndx = 1;
   }
 
@@ -110,7 +127,8 @@ void classifyImage() {
   Serial.println(maxIndx);
 }
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
 
   // Initialize random seed
@@ -119,7 +137,8 @@ void setup() {
   Serial.begin(9600);
 
   delay(5000);
-  while (!Serial);
+  while (!Serial)
+    ;
 
   // Initialize the TinyML Shield
   initializeShield();
@@ -140,42 +159,74 @@ void setup() {
   // Serial.println("Use the on-shield button to start and stop the loop code ");
   Serial.println("Training the network locally with: " + String(EPOCH_RUN) + " epochs");
 
-  for (int epoch = 0; epoch < EPOCH_RUN; epoch++) {
+  for (int epoch = 0; epoch < EPOCH_RUN; epoch++)
+  {
     do_training();
   }
 
-  
-  if (!Camera.begin(QQVGA, RGB565, 1, OV7675)) {
-    //QCIF, QQVGA
+  if (!Camera.begin(QQVGA, RGB565, 1, OV7675))
+  {
+    // QCIF, QQVGA
     Serial.println("Failed to initialize camera");
-    while (1);
+    while (1)
+      ;
   }
   bytesPerFrame = Camera.width() * Camera.height() * Camera.bytesPerPixel();
 
+  Serial.println("Starting Federated Learning between two nodes (Central and Peripheral"); 
 
-  packUnpackVector(0);
+  if (CENTRAL_OR_PERIPHERAL == 0)
+  {
+    // Central
+    Serial.println("Central Device");
+    Serial.println("Packing weights and biases for BLE transfer...");
+    packUnpackVector(0);
+    Serial.println("Packing completed. Starting BLE transfer...");
+    BLECentralSetup(WeightBiasPtr);
+    Serial.println("BLE transfer completed.");
 
-  /* for (int i = 0; i < weightBiasLength; i++) {
-    Serial.print(WeightBiasPtr[i]);
-    Serial.print(", ");
-  } */
-  // Serial.println("Test");
+    Serial.println("Swapping role to Peripheral Device...");
+    delay(5000);
 
-  // BLECentralSetup(WeightBiasPtr);
-  BLEPeripheralSetup(WeightBiasPtr);
-  Serial.println("BLE completed. Unpacking values...");
-  packUnpackVector(1);
-  printAccuracy();
+    Serial.println("Waiting for BLE transfer...");
+    BLEPeripheralSetup(WeightBiasPtr);
+    Serial.println("BLE transfer completed. Unpacking values...");
+    packUnpackVector(1); // UNPACK
+    Serial.println("Unpacking completed. Printing updated accuracy:");
+    printAccuracy();
 
-  Serial.println("Press button to take image\n");
+  }
+  else
+  {
+    // Peripheral
+    Serial.println("Peripheral Device");
+    Serial.println("Waiting for BLE transfer...");
+    BLEPeripheralSetup(WeightBiasPtr);
+    Serial.println("BLE transfer completed. Unpacking values...");
+    packUnpackVector(2); // AVERAGE, set 1 for just UNPACK
+    Serial.println("Unpacking completed. Printing updated accuracy:");
+    printAccuracy();
 
+    Serial.println("Swapping role to Central Device...");
+    delay(5000);
+    
+    Serial.println("Packing and sending weights and biases for BLE transfer...");
+    packUnpackVector(0);
+    BLECentralSetup(WeightBiasPtr);
+    Serial.println("BLE transfer completed.");
+
+  }
+
+  Serial.println("Press the button to take in image");
 }
 
-void loop() {
+void loop()
+{
 
   bool clicked = readShieldButton();
 
-  if (clicked) {
+  if (clicked)
+  {
     Serial.print("Button clicked\n");
     Camera.readFrame(image);
     convertToGrayscale(image);
